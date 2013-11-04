7494ca4216db960e813b609ae36dbe76974c2e9b
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-11-04 14:09:16 +0100

    added missing brackets

--- src/vigra_interface/vigra.cxx
+++ src/vigra_interface/vigra.cxx
@@ -9,11 +9,11 @@ namespace rpi_st7735 {
 
 
 EXIT_FLAG draw_multi_array(vigra::MultiArrayView<2, vigra::RGBValue<unsigned char> > image, const std::string& device_name) {
-  return write_sequence(device_name, image.begin(), image.end(), RGBLayoutInterpreter);
+  return write_sequence(device_name, image.begin(), image.end(), RGBLayoutInterpreter());
 }
 
 EXIT_FLAG draw_multi_array(vigra::MultiArrayView<3, unsigned char> image, const std::string& device_name) {
-  return write_sequence(device_name, image.begin(), image.end(), FlatLayoutInterpreter);
+  return write_sequence(device_name, image.begin(), image.end(), FlatLayoutInterpreter());
 }
 
 EXIT_FLAG draw_image_from_file(const std::string& filename, const std::string& device_name) {
